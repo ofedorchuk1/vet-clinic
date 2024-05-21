2b6babd5213757aa8433cc1f0504bfc6be4bbbcd
Olha Khomych <o.fedorchuk1@gmail.com>
2024-05-21 21:12:56 +0300

    REQ-08: Add registration date for Pet class

--- src/main/java/com/magicvet/comparator/Sandbox.java
+++ src/main/java/com/magicvet/comparator/Sandbox.java
@@ -1,5 +1,6 @@
 package main.java.com.magicvet.comparator;
 
+import main.java.com.magicvet.model.Cat;
 import main.java.com.magicvet.model.Dog;
 import main.java.com.magicvet.model.Pet;
 
@@ -8,22 +9,6 @@ import java.util.Comparator;
 
 public class Sandbox {
     public static void main(String[] args) {
-        Dog[] dogs = {
-                new Dog(Dog.Size.M, Pet.HealthState.GOOD),
-                new Dog(Dog.Size.S, Pet.HealthState.EXCELLENT),
-                new Dog(Dog.Size.XL, Pet.HealthState.POOR),
-                new Dog(Dog.Size.XL, Pet.HealthState.GOOD),
-                new Dog(Dog.Size.XS, Pet.HealthState.CRITICAL),
-                new Dog(Dog.Size.S, Pet.HealthState.CRITICAL)
-        };
-        Arrays.sort(dogs, new Comparator<Dog>() {
-            @Override
-            public int compare(Dog o1, Dog o2) {
-                return o1.getHealthState().getValue() - o2.getHealthState().getValue();
-            }
-        });
-        for (Dog dog : dogs){
-            System.out.println(dog.getHealthState());
-        }
+        System.out.println(new Cat());
     }
 }
